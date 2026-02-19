6d0db010a3f2526528f391f716387327b1c4ce64
Anuj Shelke <anujshelke18@gmail.com>
2026-02-19 13:59:40 +0530

    code To add two numbers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("Sum = " + sum);
     }
 }
