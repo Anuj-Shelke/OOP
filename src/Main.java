ff7e552de39f7dd70b89318b172c6f188357bb12
Anuj Shelke <anujshelke18@gmail.com>
2026-02-24 18:47:23 +0530

    Program to Print Array Elements in Reverse Order

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println(" Program to Print Array Elements in reverse Order   ");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number You Want to Search ");
+        int a = sc.nextInt();
         int marks[] = {12, 13, 13, 14, 16};
         int sum = 0;
         for (int i = 0; i < marks.length; i++) {
-            System.out.println(marks[i]);
-            sum = sum + marks[i];
+
+            if (marks[i] == a) {
+                System.out.println("ELement is found found at index : " + i);
+            }
         }
-        System.out.println("Sum is : " +sum);
-
-
     }
 }
+
+
+
+
