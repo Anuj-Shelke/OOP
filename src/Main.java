2911e006015d78b7c450554b3c6b63999e1d624d
Anuj Shelke <anujshelke18@gmail.com>
2026-02-24 18:37:32 +0530

    Starting with arrays

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Program to Print Star Pattern  ");
-        for (int i = 1; i <= 5 ; i++){
-            for (int j = 0 ; j < i ; j++){
-                System.out.print("*");
-            }
-            System.out.println();
+        System.out.println(" Program to Print Array Elements in reverse Order   ");
+        int marks[] = {12, 13, 13, 14, 16};
+        int sum = 0;
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i]);
+            sum = sum + marks[i];
         }
-
+        System.out.println("Sum is : " +sum);
 
 
     }
