88d32a787cfc60b68e6c62a90d3d38af63ace24f
Anuj Shelke <anujshelke18@gmail.com>
2026-02-24 19:30:29 +0530

    Program to add two matrics of (2*3) size

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(" Program to calculate Average marks of Students Present in an array    ");
-        double  marks[] = {12, 13, 13, 14, 16};
-        double avg = 0 ;
-        double  sum = 0;
-        for (int i = 0 ; i< 5; i++ ){
-            sum  =  (sum + marks[i]);
-        }
-        avg = sum/5;
-        System.out.println("The average is "+avg);
+        System.out.println(" Program to add two matrics of (2*3) size   ");
+        int [][] marks = {{12, 13, 14},
+                         {15, 16, 17}
+            };
+        int [][] marks1= {{16, 17, 17},
+                {17, 18, 19}};
+
+        int [][] result= new int[2][3];
+
+for(int i = 0 ; i < 2; i++){
+    for(int j = 0 ; j < 3 ; j++){
+        result[i][j] = marks[i][j]+marks1[i][j];
+    }
+}
+        System.out.println("Sum of matrices is ");
+for(int i = 0 ; i< 2 ; i++){
+    for(int j = 0 ; j< 3 ; j++){
+        System.out.print(result[i][j]+ " ");
+    }
+    System.out.println();
+}
 
 
 
