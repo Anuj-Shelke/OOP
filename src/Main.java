4113a3324df6950516e0652687e422a2c983bb54
Anuj Shelke <anujshelke18@gmail.com>
2026-02-24 22:50:00 +0530

    Program to Find minimum Element in an Array

--- src/Main.java
+++ src/Main.java
@@ -2,33 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(" Program to add two matrics of (2*3) size   ");
-        int [][] marks = {{12, 13, 14},
-                         {15, 16, 17}
-            };
-        int [][] marks1= {{16, 17, 17},
-                {17, 18, 19}};
+        System.out.println(" Program to Find minimum Element in an Array    ");
+        System.out.println("Enter the Elements ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int arr[] = new int[5];
 
-        int [][] result= new int[2][3];
+        for(int i = 0 ; i < 5 ; i++){
+            arr[i] = sc.nextInt();
+        }
+        int min = arr[0];
+        for(int i = 0 ; i < 5 ; i++){
+            if(arr[i] < min ){
+                min = arr[i];
+            }
+        }
+        System.out.println("The minimum Element is "+min);
+        }
+        }
 
-for(int i = 0 ; i < 2; i++){
-    for(int j = 0 ; j < 3 ; j++){
-        result[i][j] = marks[i][j]+marks1[i][j];
-    }
-}
-        System.out.println("Sum of matrices is ");
-for(int i = 0 ; i< 2 ; i++){
-    for(int j = 0 ; j< 3 ; j++){
-        System.out.print(result[i][j]+ " ");
-    }
-    System.out.println();
-}
-
-
-
-
-    }
-}
 
 
 
