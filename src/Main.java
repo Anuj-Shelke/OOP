30e7f2136fce92a474be5c561f33209b69fc831d
Anuj Shelke <anujshelke18@gmail.com>
2026-02-25 16:00:37 +0530

    Operations of Arrays

--- src/Main.java
+++ src/Main.java
@@ -3,38 +3,49 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(" Program Statement Implement a robust Java calculator program that captures user input dynamically,\n" +
-                "processes mathematical expressions using conditional logic and looping constructs, and\n" +
-                "ensures efficient error handling. ");
+        System.out.println(" Problem Statement Develop a Java program that performs various operations on arrays, including displaying\n" +
+                "elements, finding the maximum and minimum elements, calculating the sum and average\n" +
+                "of elements, and searching for a specific element within the array.. ");
+        int arr [] = {1,2,3,5,6};
+        System.out.println("The Array Elements Are ");
+        for(int i = 0 ; i< arr.length ; i++){
+            System.out.print(arr[i]+" ");
+        }
+        int i = 0 ;
+        int max = arr[i];
+        //Finding Maximum Elements
+        for( i = 0 ; i < arr.length ; i++){
+            if(max < arr[i]){
+                max = arr[i] ;
+            }
 
-        System.out.println("Enter Number A ");
+        }
+        System.out.println("The  Maximum Element of the Array is : "+max);
+        int min = arr[0];
+        for(i = 0 ; i < arr.length ; i++ ){
+            if(min > arr[i]){
+                min = arr[i];
+            }
+        }
+        System.out.println("The Minimum Element of the Array is :  "+min);
+
+
+        int sum = 0 ;
+        for( i = 0 ; i < arr.length ; i++){
+            sum = sum+arr[i];
+        }
+        System.out.println("Sum of Array Elements is : "+sum );
+        System.out.println("Avg of Array Elements is : "+sum/5);
+
+        // Searching Specific Element in an Array
+
+        System.out.println("Enter the element You Want to Search ");
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        System.out.println("Enter Number B ");
-        int b = sc.nextInt();
-        System.out.println("Enter 1 For Addition ");
-        System.out.println("Enter 2 For Multiplication ");
-        System.out.println("Enter 3 for Division ");
-        System.out.println("Enter 4 for Substraction ");
-        int x = sc.nextInt();
-        switch (x)
-        {
-            case 1 :
-                System.out.println("The Addition of the Numbers is "+(a+b));
-                break;
-            case 2 :
-                System.out.println("The Multiplication of Numbers is "+a*b);
-                break;
-            case 3 :
-                System.out.println("The Division of Numbers is "+a/b);
-                break;
-            case 4 :
-                System.out.println("The Substraction of Numbers is "+(a-b));
-                break;
-            default:
-                System.out.println("Enter a valid response ");
-                break;
-
+        int element = sc.nextInt();
+        for( i = 0 ; i < arr.length; i++ ){
+            if(arr[i] == element ){
+                System.out.println("The element "+ element + " is found at index : "+i);
+            }
         }
     }
 }
