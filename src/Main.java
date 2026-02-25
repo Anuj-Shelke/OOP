0c97eb4db36f44a7720c178621058e621b7c3c97
Anuj Shelke <anujshelke18@gmail.com>
2026-02-25 15:09:25 +0530

    Java Calculator

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,40 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(" Program to Find minimum Element in an Array    ");
-        System.out.println("Enter the Elements ");
+        System.out.println(" Program Statement Implement a robust Java calculator program that captures user input dynamically,\n" +
+                "processes mathematical expressions using conditional logic and looping constructs, and\n" +
+                "ensures efficient error handling. ");
+
+        System.out.println("Enter Number A ");
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        int arr[] = new int[5];
+        System.out.println("Enter Number B ");
+        int b = sc.nextInt();
+        System.out.println("Enter 1 For Addition ");
+        System.out.println("Enter 2 For Multiplication ");
+        System.out.println("Enter 3 for Division ");
+        System.out.println("Enter 4 for Substraction ");
+        int x = sc.nextInt();
+        switch (x)
+        {
+            case 1 :
+                System.out.println("The Addition of the Numbers is "+(a+b));
+                break;
+            case 2 :
+                System.out.println("The Multiplication of Numbers is "+a*b);
+                break;
+            case 3 :
+                System.out.println("The Division of Numbers is "+a/b);
+                break;
+            case 4 :
+                System.out.println("The Substraction of Numbers is "+(a-b));
+                break;
+            default:
+                System.out.println("Enter a valid response ");
+                break;
 
-        for(int i = 0 ; i < 5 ; i++){
-            arr[i] = sc.nextInt();
         }
-        int min = arr[0];
-        for(int i = 0 ; i < 5 ; i++){
-            if(arr[i] < min ){
-                min = arr[i];
-            }
-        }
-        System.out.println("The minimum Element is "+min);
-        }
-        }
-
-
-
-
-
+    }
+}
