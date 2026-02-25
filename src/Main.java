ca49190244846baa517c4a5b3bc908f1f262a366
Anuj Shelke <anujshelke18@gmail.com>
2026-02-25 17:39:45 +0530

    Method with Return Value

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,21 @@
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
+
+class addition{
+    int add(int a , int b ){
+        return (a+b);
+
+    }
+}
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(" Understanding Method Creation and Calling  ");
-        class greet{
-            void hello(){
-                System.out.println("Hello jiii ");
-            }
-        }
-        greet obj = new greet();
-        obj.hello();
+        System.out.println(" Method with return value  ");
 
+        addition obj  = new addition();
+        int result  = obj.add(4, 5 );
+        System.out.println(result);
     }
 }
