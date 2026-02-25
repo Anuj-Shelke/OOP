5b32a454017a8bce42f0f01992c99b76ff1844c4
Anuj Shelke <anujshelke18@gmail.com>
2026-02-25 19:44:53 +0530

    Program to print Star Pattern

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
-class mul {
-    void table(int num) {
-        for (int i = 0; i < 11; i++) {
-            System.out.println(num + " x " + i + "=" + (num * i));
+class star {
+    void pattern () {
+        for (int i = 0; i < 6; i++) {
+           for (int j = 0 ; j < i-1; j++){
+               System.out.print("*");
 
+           }
+            System.out.println();
         }
     }
 }
@@ -15,13 +18,11 @@ class mul {
 public class Main {
     public static void main(String[] args) {
         System.out.println(" Program to Print Multiplication Table of a Number using Method    ");
-        mul obj = new mul();
-        System.out.println("Enter the number You want the multiplication table of ");
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        star obj = new star();
 
 
-        obj.table(n);
+
+        obj.pattern();
     }
 
 
