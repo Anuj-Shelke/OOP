cc9f40a1f71e6e0826482ac5e02795017d8fcf8e
Anuj Shelke <anujshelke18@gmail.com>
2026-02-25 17:58:36 +0530

    Method using Instance varaibles

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,21 @@
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
-
-class addition{
-    int add(int a , int b ){
-        return (a+b);
-
+class student{
+    String name ;
+    int marks ;
+    void display(){
+        System.out.println("Welcome : "+name );
+        System.out.println("Yours marks are : "+marks);
     }
 }
-
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println(" Method with return value  ");
+        System.out.println(" Method using Instance Variable   ");
+        student obj = new student() ;
+        obj.name = "anuj";
+        obj.marks = 29;
+        obj.display();
 
-        addition obj  = new addition();
-        int result  = obj.add(4, 5 );
-        System.out.println(result);
     }
 }
