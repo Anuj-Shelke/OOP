fc53943909fd232e86592f207202fe6e0c2a3850
Anuj Shelke <anujshelke18@gmail.com>
2026-02-25 18:48:23 +0530

    Porgarm to Calculate Area of Square

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,28 @@
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
-class student{
-    String name ;
-    int marks ;
-    void display(){
-        System.out.println("Welcome : "+name );
-        System.out.println("Yours marks are : "+marks);
+class square{
+    int length ;
+    int breadth ;
+    int area(){
+        return (length * breadth );
     }
 }
+
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(" Method using Instance Variable   ");
-        student obj = new student() ;
-        obj.name = "anuj";
-        obj.marks = 29;
-        obj.display();
+         square obj  = new square() ;
+        obj.length = 10;
+        obj.breadth = 10 ;
+         int area = obj.area();
+
+        System.out.println("The area of the rectangle is : "+area );
+
+
+
 
     }
 }
