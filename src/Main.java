f7b6edf82bf1afa74bf6dc954c7034d32eb72742
Anuj Shelke <anujshelke18@gmail.com>
2026-03-31 10:51:07 +0530

    OOP concepts Classes and Objects

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,19 @@
-import java.util.Scanner;
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Size of Array ");
-        int n = sc.nextInt();
-        int []arr = new int[n];
-        System.out.println("Enter Array Elements ");
-        for(int i = 0 ; i< n ; i ++){
-            arr[i] = sc.nextInt();
-        }
-        System.out.println("The Elements of Array are ");
-        for (int i = 0 ; i<n ; i++){
-            System.out.println(arr[i]);
-        }
-        int min = arr[0];
-        int max = arr[0];
-        for(int i = 0 ; i< n ; i++){
-            if(arr[i]< min ){
-                min = arr[i];
-            }
-            if(arr[i]> max){
-                max = arr[i];
-            }
-        }
-        System.out.println("The minimum Element of the Array is "+min);
-        System.out.println("The Maximum Element of the Array is "+max);
-        int sum = arr[0];
-        for(int i = 0 ; i <n ; i++){
-            sum = sum + arr[i];
-        }
-        int avg = sum/n;
-        System.out.println("The Sum of the Array Elements is "+sum);
-        System.out.println("The Avg of the Array Elements is "+avg);
-        System.out.println("Enter the Element You want to search for ");
-        int num = sc.nextInt();
-        boolean found = false ;
-        for(int i =  0  ; i< n ; i++){
-            if(num == arr[i]){
-                System.out.println("The element is found at index "+i);
-                found = true ;
-            }
-        }
-        if(!found){
-            System.out.println("The element is not present in the Array ");
-        }
+import java.sql.SQLOutput;
+
+class Pen{
+    String color ;
+    String type ;
+    public void write(){
+        System.out.println("Writing Something ");
+    }
+}
+public class Main {
+public static void main(String[] args){
+    Pen pen1 = new Pen();
+    pen1.color = "red";
+    pen1.type = "Ball";
+    System.out.println(pen1.color);
+    System.out.println(pen1.type);
+    pen1.write();
     }
 }
