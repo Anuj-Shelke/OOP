b1ec49351ec453e92a2d0d950314f49300ee19ed
Anuj Shelke <anujshelke18@gmail.com>
2026-03-31 08:47:48 +0530

    Assignment 3 OOP Program To Perform Various Operations on Arrays

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,48 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
-
-class star {
-    void pattern () {
-        for (int i = 0; i < 6; i++) {
-           for (int j = 0 ; j < i-1; j++){
-               System.out.print("*");
-
-           }
-            System.out.println();
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Size of Array ");
+        int n = sc.nextInt();
+        int []arr = new int[n];
+        System.out.println("Enter Array Elements ");
+        for(int i = 0 ; i< n ; i ++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("The Elements of Array are ");
+        for (int i = 0 ; i<n ; i++){
+            System.out.println(arr[i]);
+        }
+        int min = arr[0];
+        int max = arr[0];
+        for(int i = 0 ; i< n ; i++){
+            if(arr[i]< min ){
+                min = arr[i];
+            }
+            if(arr[i]> max){
+                max = arr[i];
+            }
+        }
+        System.out.println("The minimum Element of the Array is "+min);
+        System.out.println("The Maximum Element of the Array is "+max);
+        int sum = arr[0];
+        for(int i = 0 ; i <n ; i++){
+            sum = sum + arr[i];
+        }
+        int avg = sum/n;
+        System.out.println("The Sum of the Array Elements is "+sum);
+        System.out.println("The Avg of the Array Elements is "+avg);
+        System.out.println("Enter the Element You want to search for ");
+        int num = sc.nextInt();
+        boolean found = false ;
+        for(int i =  0  ; i< n ; i++){
+            if(num == arr[i]){
+                System.out.println("The element is found at index "+i);
+                found = true ;
+            }
+        }
+        if(!found){
+            System.out.println("The element is not present in the Array ");
         }
     }
 }
-
-
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(" Program to Print Multiplication Table of a Number using Method    ");
-        star obj = new star();
-
-
-
-        obj.pattern();
-    }
-
-
-
-
-
-
-
-
-    }
-
