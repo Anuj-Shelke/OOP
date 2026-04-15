7f41fe05014681a16c77d7b424a68c4710438ec4
Anuj Shelke <anujshelke18@gmail.com>
2026-04-15 09:15:57 +0530

    Java code to represent Polymorphism

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,24 @@
-import java.sql.SQLOutput;
-import java.util.Scanner;
-class Product{
-    int id ;
-    String name ;
-    int price ;
-    int qty;
-
-    //Constructor
-    Product(int id , String name, int price , int qty){
-        this.id = id ;
-        this.name = name ;
-        this.price = price;
-        this.qty = qty;
-
-
-        }
-    double total(){
-        return price*qty;
+interface animal {
+    void sound();
+}
+class dog implements animal{
+    public void sound(){
+        System.out.println("Dog Barks");
     }
-
+}
+class cat implements animal{
+    public void sound(){
+        System.out.println("Cat meows ");
+    }
 }
 public class Main{
     public static void main(String[] args){
-        Scanner  sc = new Scanner (System.in);
-        System.out.println("Enter Number of Products ");
-        int n = sc.nextInt();
-        double grandtotal = 0;
-
-        System.out.println("Enter Product Details");
-        for (int i = 0 ; i< n ; i++){
-            System.out.println("Enter ID ");
-            int id = sc.nextInt();
-            sc.nextLine();
-
-            System.out.println("Enter Name ");
-            String name = sc.nextLine();
-
-            System.out.println("Enter Price ");
-            int price = sc.nextInt();
-
-            System.out.println("Enter Quantity ");
-            int qty = sc.nextInt();
-
-            Product p = new Product(id , name , price, qty);
-            grandtotal += p.total();
-
-        }
-        double discount = 0 ;
-        if(grandtotal > 10000){
-            discount = grandtotal* 0.10 ;
-        }
-        else if(grandtotal > 5000){
-            discount = grandtotal*20 ;
-        }
-        double final_amount = grandtotal - discount;
-        System.out.println("==========Invoice============");
-        System.out.println("Total : "+grandtotal);
-        System.out.println("discount : "+discount);
-        System.out.println("final amount : "+final_amount);
-        sc.close();
+        animal a = new dog();
+        a.sound();
+        a = new cat();
+        a.sound();
     }
+
 }
+
 
